23fc5cb8adfb281e21c498fa9a31c182f8a09357
niilliiz <niloo.vatan.nv@@gmail.com>
2024-01-04 08:18:02 +0330

    map

--- p5-playlist/sketch.js
+++ p5-playlist/sketch.js
@@ -6,13 +6,11 @@
 let circleX = 100;
 let unit = 5;
 let r, g, b;
+let color = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, 400);
   background(120, 250, 0);
-  r = random(255);
-  g = random(255);
-  b = random(255);
 }
 
 function mousePressed() {
@@ -23,8 +21,16 @@ function mousePressed() {
 }
 
 function draw() {
+  color = map(mouseX, 0, width, 0, 255);
+  // color = mouseX;
+  background(color);
+
   noStroke();
-  fill(r, g, b, 0.5);
+  fill(125, 25, 62);
+
+  if (mouseX % 11 === 0) {
+    fill(250, 12, 98);
+  }
   ellipse(mouseX, mouseY, 50, 50);
 }
 
